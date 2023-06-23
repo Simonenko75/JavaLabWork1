db09f9b644b1c44219d4f5ff2235d884fe65889e
Simonenko75 <simonenkonikita00@gmail.com>
2023-06-23 13:21:21 +0300

    The end of lab-work

--- src/main/java/com/example/LabWork1/model/DocumentEntity.java
+++ src/main/java/com/example/LabWork1/model/DocumentEntity.java
@@ -2,7 +2,6 @@ package com.example.LabWork1.model;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
 import java.time.Instant;
